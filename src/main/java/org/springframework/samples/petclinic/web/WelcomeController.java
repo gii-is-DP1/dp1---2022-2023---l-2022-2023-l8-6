bdfc801ce49fae4bb6f241697aa1a0466b62513c
Adrián García Chavero <adrgarcha@alum.us.es>
2022-11-13 21:01:59 +0100

    Modificado el apartado de Welcome

--- src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/web/WelcomeController.java
@@ -1,7 +1,10 @@
 package org.springframework.samples.petclinic.web;
 
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.samples.petclinic.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,8 +13,16 @@ public class WelcomeController {
 	
 	
 	  @GetMapping({"/","/welcome"})
-	  public String welcome(Map<String, Object> model) {	    
-
+	  public String welcome(Map<String, Object> model) {	
+		
+		List<Person> personas = new ArrayList<Person>();
+		Person persona = new Person();
+		persona.setFirstName("Adrian");
+		persona.setLastName("Garcia");
+		personas.add(persona);
+		model.put("persons", personas);
+		model.put("title", "Buscaminas");
+		model.put("group", "L8-6");
 	    return "welcome";
 	  }
 }
